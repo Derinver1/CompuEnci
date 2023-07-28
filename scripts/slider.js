6da753668e6be79d5e59cf62c7963dc507b502dd
Derinver1 <130020120+Derinver1@users.noreply.github.com>
2023-07-28 14:05:00 -0600

    Add files via upload

---
+++ scripts/slider.js
@@ -0,0 +1,7 @@
+const slider0=document.getElementById("slider0");
+const slider1=document.getElementById("slider1");
+const slider2=document.getElementById("slider2");
+
+slider1.addEventListener("animationend",()=>{
+	slider1.classList.remove("der");
+});
